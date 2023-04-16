7c13b1b3c353b3249456a5113208de56a21ab868
dima <dima.xipkov@gmail.com>
2023-04-17 00:21:43 +0300

    11 lesson

--- src/App.js
+++ src/App.js
@@ -6,29 +6,41 @@
 
 // map - добовляет что то к каждому элементу
 const foodILike = [{
-    'name': 'борщ',
+    'id':1,
+    'name': 'рщ',
     'image': 'https://lifehacker.ru/wp-content/uploads/2014/12/ob-05_1568611223.jpg'
   },
   {
+    'id': 2,
     'name': 'драники',
     'image': 'https://lifehacker.ru/wp-content/uploads/2014/12/ob-05_1568611223.jpg'
   },
   {
+    'id': 3,
     'name': 'стейк',
     'image': 'https://lifehacker.ru/wp-content/uploads/2014/12/ob-05_1568611223.jpg'
   },
   {
+    'id': 4,
     'name': 'хинкали',
     'image': 'https://lifehacker.ru/wp-content/uploads/2014/12/ob-05_1568611223.jpg'
   }
 ]
 
 // dish-блюдо текуший элемент масива
+
+
+function renderFood(dish) {
+  return <Food key={dish.id} name={dish.name} picture={dish.image}/>
+}
+
 function App() {
   return (
     <div>
+      {console.log(foodILike.map(renderFood))}
       {/* <h1>Привет</h1>  */}
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
+      {/* {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)} */}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
